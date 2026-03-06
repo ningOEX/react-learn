3ff56a4fd60c2cec6f9308fc16c01125454566da
studentsning <2300382007@qq.com>
2026-03-06 15:43:12 +0800

    init

--- src/components/MovieList.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Movie from "./Movie";
-import "./Movie.css"
-
-export default function MovieList(props) {
-  const movies = props.movies.map((item, i) => <Movie key={i} {...item} />);
-  return <ul className="movie_container">{movies}</ul>;
-}
