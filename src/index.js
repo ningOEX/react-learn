c254a6ce165a22345da2898e27534995b422d1f1
studentsning <2300382007@qq.com>
2026-03-06 15:45:35 +0800

    demo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import "./index.css"
 import ReactDOM from "react-dom/client";
-import PagerTest from "./components/PagerTest";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
  root.render(
   <React.StrictMode>
-    {/* <BallList /> */}
-   <PagerTest />
+    <h1>1</h1>
   </React.StrictMode>
   )
